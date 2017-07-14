cfbb395fa5f484403d9ffd944ea7ecb721410da0
Rafael <rapha.pse@outlook.com>
2017-07-13 22:36:18 -0300

    Adicionando projeto T

---
+++ bankline/src/main/java/com/bk/bankline/BanklineApplication.java
@@ -0,0 +1,12 @@
+package com.bk.bankline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanklineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanklineApplication.class, args);
+	}
+}
