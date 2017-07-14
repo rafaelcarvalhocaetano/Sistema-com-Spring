93e76b1527b8bccbd1e60c2e98330e368140c062
Rafael <rapha.pse@outlook.com>
2017-07-13 22:36:45 -0300

    Adicionando projeto T

---
+++ cob/src/main/java/com/teste/cob/CobApplication.java
@@ -0,0 +1,12 @@
+package com.teste.cob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobApplication.class, args);
+	}
+}
