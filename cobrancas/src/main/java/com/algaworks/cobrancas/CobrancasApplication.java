03e4375b39d99d5d9cf903e95d3e7c563f7c3987
Rafael <rapha.pse@outlook.com>
2017-07-14 21:03:01 -0300

    Adicionando projeto cobranças

---
+++ cobrancas/src/main/java/com/algaworks/cobrancas/CobrancasApplication.java
@@ -0,0 +1,12 @@
+package com.algaworks.cobrancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobrancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobrancasApplication.class, args);
+	}
+}
