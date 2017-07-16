f498cf26d85a8cc00f96874811711472fdc3d805
Rafael <rapha.pse@outlook.com>
2017-07-16 12:14:39 -0300

    Adicionando projeto cob

--- cob/src/main/java/com/teste/cob/controller/TituloController.java
+++ cob/src/main/java/com/teste/cob/controller/TituloController.java
@@ -2,22 +2,26 @@ package com.teste.cob.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.teste.cob.model.Titulo;
 
 @Controller
+@RequestMapping("/titulos")
 public class TituloController {
 	
 
-	@RequestMapping("/titulos/novo")
+	@RequestMapping("/novo")
 	public String novo(){
 		return "CadastroTitulo";
 	}
 	
-	/*
-	@RequestMapping(value = "/titulos", method=RequestMethod.POST)
+	
+	@RequestMapping(method=RequestMethod.POST)
 	public String salvar(Titulo titulo){
 		System.out.println(">>>>> teste "+titulo.getDescricao());
 		
 		return "CadastroTitulo";
 	}
-	*/
+	
 }
