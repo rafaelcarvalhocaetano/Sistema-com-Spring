a4776c8cb96fad50eeb65980fc7e953794d1ffed
Rafael <rapha.pse@outlook.com>
2017-07-15 23:41:38 -0300

    Adicionando projeto cobranças

--- cobrancas/src/main/java/com/algaworks/cobrancas/controller/TituloController.java
+++ cobrancas/src/main/java/com/algaworks/cobrancas/controller/TituloController.java
@@ -2,12 +2,28 @@ package com.algaworks.cobrancas.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@Controller
-public class TituloController {
+
+import com.algaworks.cobrancas.model.Titulo;
+
 	
-	@RequestMapping("/titulos/novo")
+@Controller
+@RequestMapping("/titulos")
+public class TituloController {
+
+	
+	//@RequestMapping("/titulos/novo") --> após ter adicionado o /titulos
+	@RequestMapping("/novo")
 	public String novo(){
+		return "CadastroTitulo";
+	}
+	
+	//@RequestMapping(value = "/titulos", method= RequestMethod.POST) --> após ter adicionado o /titulos
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(Titulo titulo) {
+		System.out.println(">>> " + titulo.getDescricao());
+		
 		return "CadastroTitulo";
 	}
 
