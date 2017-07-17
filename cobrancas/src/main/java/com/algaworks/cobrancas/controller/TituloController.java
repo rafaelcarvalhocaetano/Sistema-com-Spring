bbefdb7314d16c8e17cdcc8a7188dc05388ca841
Rafael <rapha.pse@outlook.com>
2017-07-17 12:16:38 -0300

    Adicionando projeto financeiro com Spring

--- cobrancas/src/main/java/com/algaworks/cobrancas/controller/TituloController.java
+++ cobrancas/src/main/java/com/algaworks/cobrancas/controller/TituloController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 
 
+import org.springframework.web.servlet.ModelAndView;
+
 import com.algaworks.cobrancas.model.Titulo;
 import com.algaworks.cobrancas.repository.Titulos;
 
@@ -28,11 +30,14 @@ public class TituloController {
 	
 	//@RequestMapping(value = "/titulos", method= RequestMethod.POST) --> após ter adicionado o /titulos
 	@RequestMapping(method = RequestMethod.POST)
-	public String salvar(Titulo titulo) {
-		System.out.println(">>> " + titulo.getDescricao());
+	public ModelAndView salvar(Titulo titulo) {
 		
+		System.out.println("Salvo com sucesso "+titulo.getCodigo());
 		titulos.save(titulo);
-		return "CadastroTitulo";
+		ModelAndView mv = new ModelAndView("CadastroTitulo"); //chama a pagina CadastroTitulo através do mv
+		mv.addObject("mensagem","Título salvo com sucesso"); //cria a variavel mensagem que está no html através do themeleaf
+		
+		return mv;
 	}
 
 }
