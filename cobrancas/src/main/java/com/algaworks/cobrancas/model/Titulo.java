59170a974ede91e641daa61caff0b2a5b0af2e9b
Rafael <rapha.pse@outlook.com>
2017-07-16 22:25:56 -0300

    Adicionando projeto financeiro com Spring

--- cobrancas/src/main/java/com/algaworks/cobrancas/model/Titulo.java
+++ cobrancas/src/main/java/com/algaworks/cobrancas/model/Titulo.java
@@ -10,6 +10,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.NumberFormat;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -27,6 +28,7 @@ public class Titulo {
 	@Temporal(TemporalType.DATE)
 	private Date dataVencimento;
 	
+	@NumberFormat(pattern = "#,##0.00")
 	private BigDecimal valor;
 	
 	@Enumerated(EnumType.STRING)
