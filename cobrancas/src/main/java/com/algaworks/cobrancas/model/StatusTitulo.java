b804f08f8ae67991c3f4fbf20a2ea147840fe601
Rafael <rapha.pse@outlook.com>
2017-07-17 12:38:52 -0300

    Adicionando projeto financeiro com Spring

--- cobrancas/src/main/java/com/algaworks/cobrancas/model/StatusTitulo.java
+++ cobrancas/src/main/java/com/algaworks/cobrancas/model/StatusTitulo.java
@@ -3,7 +3,8 @@ package com.algaworks.cobrancas.model;
 public enum StatusTitulo {
 	
 	PENDENTE("Pendente"),
-	RECEBIDO("Recebido");
+	RECEBIDO("Recebido"),
+	CANCELADO("Cancelado");
 	
 	private String descricao;
 	
