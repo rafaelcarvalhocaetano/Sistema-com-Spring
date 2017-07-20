7957a0b60cf562621c2937307cb9fcca6f24aea4
Rafael <rapha.pse@outlook.com>
2017-07-20 20:58:38 -0300

    Adicionando alt

--- cobrancas/src/main/java/com/algaworks/cobrancas/controller/TituloController.java
+++ cobrancas/src/main/java/com/algaworks/cobrancas/controller/TituloController.java
@@ -88,5 +88,129 @@ public class TituloController {
 		 * 
 		*/
 	}
+	//----------------------------------------------
+private static final String CADASTRO_VIEW = "CadastroTitulo";
+	
+	@Autowired
+	private Titulos titulos;
+
+	
+	//@RequestMapping("/titulos/novo") --> após ter adicionado o /titulos
+	@RequestMapping("/novo")
+	public ModelAndView novo(){
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		//mv.addObject("todoStatusTitulo",StatusTitulo.values());
+		mv.addObject(new Titulo());
+		return mv;
+	}
+	
+	//@RequestMapping(value = "/titulos", method= RequestMethod.POST) --> após ter adicionado o /titulos
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) { //O @Validated exige o preenchimento do campo
+		//ModelAndView mv = new ModelAndView(); //chama a pagina CadastroTitulo através do mv
+		if(errors.hasErrors()){
+			return CADASTRO_VIEW;
+		}
+		//System.out.println("Salvo com sucesso "+titulo.getDescricao());
+		titulos.save(titulo);
+		//ModelAndView mv2 =  new ModelAndView("redirect:/titulos/novo"); //cria a variavel mensagem que está no html através do themeleaf
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso");
+		return "redirect:/titulos/novo";
+	}
+	
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo){		
+		//Titulo titulo = titulos.findOne(codigo); //recupera o id do titulo do banco de dados que está no model
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+		return mv;
+	}
+	
+	@RequestMapping
+	public ModelAndView pesquisar(){
+		List<Titulo> todosTitulos = titulos.findAll();
+		ModelAndView mv = new ModelAndView("PesquisaTitulos");
+		mv.addObject("titulos", todosTitulos); //disponibiliza uma variavel titulos para ser usado no html listando os itens
+		return mv; 
+	}
+	
+	@RequestMapping(value = "{codigo}", method = RequestMethod.DELETE)
+	public String excluir(@PathVariable Long codigo){
+		titulos.delete(codigo);
+		
+		return "redirect:/titulos";
+	}
+	
+	@ModelAttribute("todoStatusTitulo")
+	public List<StatusTitulo> todosTitulos(){
+		return Arrays.asList(StatusTitulo.values());
+		/*
+		 *Este método chama todos os atributos do enum sem a necessidade de duplicar o código no ModelAndView
+		 *@ModelAttribute serve para dar um nome ao metodo ao ser chamado no html ou melhor no thymeleaf 
+		 * 
+		*/
+	}
+	//---------------------------------
+private static final String CADASTRO_VIEW = "CadastroTitulo";
+	
+	@Autowired
+	private Titulos titulos;
+
+	
+	//@RequestMapping("/titulos/novo") --> após ter adicionado o /titulos
+	@RequestMapping("/novo")
+	public ModelAndView novo(){
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		//mv.addObject("todoStatusTitulo",StatusTitulo.values());
+		mv.addObject(new Titulo());
+		return mv;
+	}
+	
+	//@RequestMapping(value = "/titulos", method= RequestMethod.POST) --> após ter adicionado o /titulos
+	@RequestMapping(method = RequestMethod.POST)
+	public String salvar(@Validated Titulo titulo, Errors errors, RedirectAttributes attributes) { //O @Validated exige o preenchimento do campo
+		//ModelAndView mv = new ModelAndView(); //chama a pagina CadastroTitulo através do mv
+		if(errors.hasErrors()){
+			return CADASTRO_VIEW;
+		}
+		//System.out.println("Salvo com sucesso "+titulo.getDescricao());
+		titulos.save(titulo);
+		//ModelAndView mv2 =  new ModelAndView("redirect:/titulos/novo"); //cria a variavel mensagem que está no html através do themeleaf
+		attributes.addFlashAttribute("mensagem", "Título salvo com sucesso");
+		return "redirect:/titulos/novo";
+	}
+	
+	@RequestMapping("{codigo}")
+	public ModelAndView edicao(@PathVariable("codigo") Titulo titulo){		
+		//Titulo titulo = titulos.findOne(codigo); //recupera o id do titulo do banco de dados que está no model
+		ModelAndView mv = new ModelAndView(CADASTRO_VIEW);
+		mv.addObject(titulo);
+		return mv;
+	}
+	
+	@RequestMapping
+	public ModelAndView pesquisar(){
+		List<Titulo> todosTitulos = titulos.findAll();
+		ModelAndView mv = new ModelAndView("PesquisaTitulos");
+		mv.addObject("titulos", todosTitulos); //disponibiliza uma variavel titulos para ser usado no html listando os itens
+		return mv; 
+	}
+	
+	@RequestMapping(value = "{codigo}", method = RequestMethod.DELETE)
+	public String excluir(@PathVariable Long codigo){
+		titulos.delete(codigo);
+		
+		return "redirect:/titulos";
+	}
+	
+	@ModelAttribute("todoStatusTitulo")
+	public List<StatusTitulo> todosTitulos(){
+		return Arrays.asList(StatusTitulo.values());
+		/*
+		 *Este método chama todos os atributos do enum sem a necessidade de duplicar o código no ModelAndView
+		 *@ModelAttribute serve para dar um nome ao metodo ao ser chamado no html ou melhor no thymeleaf 
+		 * 
+		*/
+	}
 
 }
