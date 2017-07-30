6966334420d7283983897bdbd0c962f1f0d4f730
Rafael <rapha.pse@outlook.com>
2017-07-30 20:54:50 -0300

    Adicionando test-cobranca

---
+++ test-cobranca/src/main/java/com/teste/rcc/cobranca/TestCobrancaApplication.java
@@ -0,0 +1,12 @@
+package com.teste.rcc.cobranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCobrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCobrancaApplication.class, args);
+	}
+}
---
+++ test-cobranca/src/main/resources/static/js/cobrancas.js
@@ -0,0 +1,62 @@
+$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event){
+	
+	var button = $(event.relatedTarget);
+	
+	var codigoTitulo = button.data('codigo');
+	var descricaoTitulo = button.data('descricao');
+	
+	var modal = $(this);
+	var form = modal.find('form');
+	var action = form.data('url-base');
+	if (!action.endsWith('/')) {
+		action += '/';
+	}
+	form.attr('action', action + codigoTitulo);
+	
+	modal.find('.modal-body span').html('Tem certeza que deseja excluir o título <strong>' + descricaoTitulo + '</strong>?');
+	
+});
+
+//inicializa o tooltip do bootstrap após a pagina ser inicializada
+$(function name() {
+	$('[rel= "tooltip"]').tooltip();
+	
+	//configurando o compo de valor com jquery maskMoney
+	$('.js-currency').maskMoney({
+		decimal: ',',
+		thousands: '.',
+		allowZero: true
+	});
+	
+	//ação do click do botao receber
+	$('.js-atualizar-status').on('click', function(event){
+		
+		//informando para não fazer o corportamento padrão do link
+		event.preventDefault();
+		
+		var botaoReceber = $(event.currentTarget);
+		var urlReceber = botaoReceber.attr('href');
+		
+		
+		var response = $.ajax({
+			url: urlReceber,
+			type: 'PUT'
+		});
+		
+		response.done(function (e) {
+			var codigoTitulo = botaoReceber.data('codigo');
+			$('[data-role='+codigoTitulo+']').html('<span class="label label-success">'+ e +'</span>');
+			botaoReceber.hide();
+		});
+		
+		response.fail(function (e) {
+			console.log(e);
+			alert('Erro recebendo combranças');
+		});
+	
+	
+		
+	
+	});	
+});
+
