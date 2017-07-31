72d8cc76e104e92a6e71955bda12d708d9f2ee58
Rafael <rapha.pse@outlook.com>
2017-07-30 22:38:12 -0300

    Add projeto formulario financeiro

---
+++ formulario/src/main/java/com/teste/cobranca/controller/TituloController.java
@@ -0,0 +1,14 @@
+package com.teste.cobranca.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TituloController {
+	
+	@RequestMapping("/titulo/novo")
+	public String novo(){
+		return "TituloCadastro";
+	}
+
+}
